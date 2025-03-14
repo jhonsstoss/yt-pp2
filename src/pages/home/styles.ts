9976362740b546321fa52b6a1dedcc0f902dcffa
Jhonatas Santos <grigorio.ssantos@gmail.com>
2025-03-14 10:49:15 -0300

    static main page finished

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,16 +1,23 @@
 import styled from "styled-components";
 
-
-export const Container = styled.div<{ openMenu: boolean }>`
+export const BigHome = styled.div`
   width: 100%;
   max-width: 1600px;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+`;
+
+export const Container = styled.div<{ openMenu: boolean }>`
   display: grid;
-  
   column-gap: 20px;
   row-gap: 50px;
   transition: all 0.3s ease-in-out;
+  margin: 45px 45px 0 20px;
   ${({ openMenu }) => openMenu 
-    ? "grid-template-columns: repeat(5, 1fr);" 
-    : "grid-template-columns: repeat(4, 1fr);"
+    ? "grid-template-columns: repeat(4, 1fr);" 
+    : "grid-template-columns: repeat(5, 1fr);"
   }
 `;
+
